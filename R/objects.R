912114a4bc047b4b4ee46f9168eee35c629b626f
benjaminguinaudeau <bguinaudeau@gmail.com>
2020-06-08 18:50:58 -0400

    add docker signature

--- R/objects.R
+++ R/objects.R
@@ -1,10 +1,10 @@
 #' get_username
 #' @description Function to get information on a given username
 #' @export
-get_username <- function(username){
+get_username <- function(username, port = NULL){
   url <- get_url("username", username = username)
   # br <- py$browser(url)
-  res <- get_data(url)
+  res <- get_data(url, port = port)
 
   if(!"userInfo" %in% names(res)){return(tibble::tibble())}
 
@@ -18,10 +18,10 @@ get_username <- function(username){
 #' get_hashtag
 #' @description Function to get information on a given hashtag
 #' @export
-get_hashtag <- function(hashtag){
+get_hashtag <- function(hashtag, port = NULL){
   url <- get_url("hashtag", hashtag = hashtag)
   # br <- py$browser(url)
-  res <- get_data(url)
+  res <- get_data(url, port = port)
   out <- res %>%
     rlist::list.flatten() %>%
     purrr::imap_dfc(~purrr::set_names(tibble::tibble(.x), .y))
@@ -32,10 +32,10 @@ get_hashtag <- function(hashtag){
 #' get_music
 #' @description Function to get information on a given piece of music
 #' @export
-get_music <- function(music_id){
+get_music <- function(music_id, port = NULL){
   url <- get_url("music", music_id = music_id)
   # br <- py$browser(url)
-  res <- get_data(url)
+  res <- get_data(url, port = port)
   out <- res %>%
     rlist::list.flatten() %>%
     purrr::imap_dfc(~purrr::set_names(tibble::tibble(.x), .y))
