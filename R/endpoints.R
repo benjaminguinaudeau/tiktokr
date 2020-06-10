d98ef9e8a89ea3fa4cba24c31857fa5bbe217454
benjaminguinaudeau <bguinaudeau@gmail.com>
2020-06-10 16:09:07 -0400

    coherence of arguments

--- R/endpoints.R
+++ R/endpoints.R
@@ -9,27 +9,12 @@ get_trending <- function(n = 10000, ua = default_ua) get_n("trending", n = n, ua
 #' @param username Character indicating the username to scrape
 #' @param n Numeric indicating the number of tiktoks to scrape
 #' @export
-<<<<<<< HEAD
-<<<<<<< HEAD
 get_user_post <- function(username, n = 10000, save_dir = NULL, port = NULL, ua = default_ua, ...){
   user <- get_username(username, port = port, ua = ua)
 
   if(length(user) == 0){message(glue::glue("{username} was not found")) ; return(tibble::tibble())}
 
   get_n(type = "user_post", n = n, user_id = user$id, sec_uid = user$secUid, query = username, save_dir = save_dir, port = port, ua = ua, ...)
-=======
-get_user_post <- function(count, username, ...){
-  user <- get_username(username)
-  get_count(type = "user_post", count = count, user_id = user$id, sec_uid = user$secUid, query = username, ...)
->>>>>>> 16eb9ca88d97851895eba630e32e62f679ed8cf8
-=======
-get_user_post <- function(count, username, save = F, path = NULL, port = NULL, ...){
-  user <- get_username(username, port = port)
-
-  if(length(user) == 0){message(glue::glue("{username} was not found")) ; return(tibble::tibble())}
-
-  get_count(type = "user_post", count = count, user_id = user$id, sec_uid = user$secUid, query = username, save = save, path = path, port = port, ...)
->>>>>>> 5aea75f85e35f9e94fcecf3b8ebdf3d43888f62d
 }
 
 #' get_music_post
@@ -37,21 +22,9 @@ get_user_post <- function(count, username, save = F, path = NULL, port = NULL, .
 #' @param music_id Character indicating the \code{music_id} to scrape
 #' @param n Numeric indicating the number of tiktoks to scrape
 #' @export
-<<<<<<< HEAD
-<<<<<<< HEAD
 get_music_post <- function(music_id, n = 10000, port = NULL, ua = default_ua,  ...){
   # user <- get_username(username)
   get_n("music_post", n = n, music_id = music_id, query = music_id, port = port, ua = ua, ...)
-=======
-get_music_post <- function(count, music_id, ...){
-  # user <- get_username(username)
-  get_count("music_post", count = count, music_id = music_id, query = music_id, ...)
->>>>>>> 16eb9ca88d97851895eba630e32e62f679ed8cf8
-=======
-get_music_post <- function(count, music_id, port = NULL, ...){
-  # user <- get_username(username)
-  get_count("music_post", count = count, music_id = music_id, query = music_id, port = port,...)
->>>>>>> 5aea75f85e35f9e94fcecf3b8ebdf3d43888f62d
 }
 
 #' get_hashtag_post
@@ -59,24 +32,11 @@ get_music_post <- function(count, music_id, port = NULL, ...){
 #' @param hashtag Hashtag to scrape
 #' @param n Number of tiktoks to retrieve
 #' @export
-<<<<<<< HEAD
-<<<<<<< HEAD
 get_hashtag_post <- function(hashtag, n = 10000, cursor = 0, save_dir = NULL,  port = NULL, ua = default_ua){
   hash <- get_hashtag(hashtag, port = port, ua = ua)
 
   get_n(type = "hashtag_post", n = n, cursor = cursor, hash_id = hash$challengeInfo.challenge.id, query = hashtag,
         save_dir = save_dir, port = port, ua = ua )
-=======
-get_hashtag_post <- function(count, hashtag, ...){
-  hash <- get_hashtag(hashtag)
-  get_count("hashtag_post", count = count, hash_id = hash$challengeInfo.challenge.id, query = hashtag, ...)
->>>>>>> 16eb9ca88d97851895eba630e32e62f679ed8cf8
-=======
-get_hashtag_post <- function(count, hashtag, cursor = 0, save = F, path = NULL,  port = NULL, ...){
-  hash <- get_hashtag(hashtag, port = port)
-  get_count(type = "hashtag_post", count = count, cursor = cursor, hash_id = hash$challengeInfo.challenge.id, query = hashtag,
-            save = save, path = path, port = port, ...)
->>>>>>> 5aea75f85e35f9e94fcecf3b8ebdf3d43888f62d
 }
 
 #' discover_hashtags
--- R/objects.R
+++ R/objects.R
@@ -1,8 +1,6 @@
 #' get_username
 #' @description Function to get information on a given username
 #' @export
-<<<<<<< HEAD
-<<<<<<< HEAD
 get_username <- function(username, ua = ua_default, port = NULL){
   url <- get_url("username", username = username)
 
@@ -10,21 +8,6 @@ get_username <- function(username, ua = ua_default, port = NULL){
 
   if(!"userInfo" %in% names(res)){return(tibble::tibble())}
 
-=======
-get_username <- function(username){
-  url <- get_url("username", username = username)
-  # br <- py$browser(url)
-  res <- get_data(url)
->>>>>>> 16eb9ca88d97851895eba630e32e62f679ed8cf8
-=======
-get_username <- function(username, port = NULL){
-  url <- get_url("username", username = username)
-  # br <- py$browser(url)
-  res <- get_data(url, port = port)
-
-  if(!"userInfo" %in% names(res)){return(tibble::tibble())}
-
->>>>>>> 5aea75f85e35f9e94fcecf3b8ebdf3d43888f62d
   out <- res$userInfo$user %>%
     rlist::list.flatten() %>%
     purrr::imap_dfc(~purrr::set_names(tibble::tibble(.x), .y))
@@ -35,25 +18,11 @@ get_username <- function(username, port = NULL){
 #' get_hashtag
 #' @description Function to get information on a given hashtag
 #' @export
-<<<<<<< HEAD
-<<<<<<< HEAD
 get_hashtag <- function(hashtag, port = NULL, ua = ua_default){
   url <- get_url("hashtag", hashtag = hashtag)
 
   res <- get_data(url, port = port, ua = ua)
 
-=======
-get_hashtag <- function(hashtag){
-  url <- get_url("hashtag", hashtag = hashtag)
-  # br <- py$browser(url)
-  res <- get_data(url)
->>>>>>> 16eb9ca88d97851895eba630e32e62f679ed8cf8
-=======
-get_hashtag <- function(hashtag, port = NULL){
-  url <- get_url("hashtag", hashtag = hashtag)
-  # br <- py$browser(url)
-  res <- get_data(url, port = port)
->>>>>>> 5aea75f85e35f9e94fcecf3b8ebdf3d43888f62d
   out <- res %>%
     rlist::list.flatten() %>%
     purrr::imap_dfc(~purrr::set_names(tibble::tibble(.x), .y))
@@ -64,25 +33,11 @@ get_hashtag <- function(hashtag, port = NULL){
 #' get_music
 #' @description Function to get information on a given piece of music
 #' @export
-<<<<<<< HEAD
-<<<<<<< HEAD
 get_music <- function(music_id, port = NULL, ua = ua_default){
   url <- get_url("music", music_id = music_id)
 
   res <- get_data(url, port = port, ua = ua)
 
-=======
-get_music <- function(music_id){
-  url <- get_url("music", music_id = music_id)
-  # br <- py$browser(url)
-  res <- get_data(url)
->>>>>>> 16eb9ca88d97851895eba630e32e62f679ed8cf8
-=======
-get_music <- function(music_id, port = NULL){
-  url <- get_url("music", music_id = music_id)
-  # br <- py$browser(url)
-  res <- get_data(url, port = port)
->>>>>>> 5aea75f85e35f9e94fcecf3b8ebdf3d43888f62d
   out <- res %>%
     rlist::list.flatten() %>%
     purrr::imap_dfc(~purrr::set_names(tibble::tibble(.x), .y))
