d0b9ccaa1d9e95a2d7522bb3f5c485da8260c61a
benjaminguinaudeau <bguinaudeau@gmail.com>
2020-06-10 21:35:26 -0400

    fix missing argument

--- R/endpoints.R
+++ R/endpoints.R
@@ -14,13 +14,13 @@ tk_posts <- function(scope, query, n = 10000, cursor = 0, save_dir = NULL, port 
   switch(
     scope,
     "user" = {
-      user <- tk_info(scope = scope, query)
+      user <- tk_info(scope = scope, query, port = port, ua = ua)
       if(length(user) == 0){message(glue::glue("{query} was not found")) ; return(tibble::tibble())}
       get_n(type = "user_post", n = n, query_1 = user$id, query_2 = user$secUid, query = query,
                       save_dir = save_dir, port = port, ua = ua)
     },
     "hashtag" = {
-      hash <- tk_info(scope = scope, query)
+      hash <- tk_info(scope = scope, query, port = port, ua = ua)
       ## TODO: hash$challengeInfo.challenge.id this probably needs to be a better variable name!
       get_n(type = "hashtag_post", n = n, cursor = cursor, query_1 = hash$challengeInfo.challenge.id, query = query,
                       save_dir = save_dir, port = port, ua = ua )
