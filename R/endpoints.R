8769f21f4748e673598bd5ceb30cdeaf1c3f2afc
benjaminguinaudeau <bguinaudeau@gmail.com>
2020-07-30 10:20:13 -0400

    fix start_date

--- R/endpoints.R
+++ R/endpoints.R
@@ -23,7 +23,8 @@ tk_posts <- function(scope, query, n = 10000, start_date = lubridate::dmy("01-01
         } else {
           get_n(type = "user_post", n = n, start_date = start_date, query_1 = user$user.id, query_2 = user$user.secUid, query = query,
                 save_dir = save_dir, port = port, ua = ua, vpn = vpn) %>%
-            bind_cols(user)
+            dplyr::bind_cols(user) %>%
+            dplyr::filter(from_unix(createTime) > start_date)
         }
       },
       "hashtag" = {
@@ -48,7 +49,7 @@ tk_posts <- function(scope, query, n = 10000, start_date = lubridate::dmy("01-01
   } else {
     if(verbose){
       if(!any(str_detect(names(out), "stats"))){
-        cli::cli_alert_info("[{Sys.time()}] {stringr::str_extract(scope, '.')}-{query} (not found)")
+        cli::cli_alert_warning("[{Sys.time()}] {stringr::str_extract(scope, '.')}-{query} (not found)")
       } else if (unique(out$stats.videoCount) == 0){
         cli::cli_alert_info("[{Sys.time()}] {stringr::str_extract(scope, '.')}-{query} (no videos)")
       } else if(unique(out$user.secret)){
