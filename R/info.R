5bd12cc1174e9e053a2e39021e286f99d2eb6e03
Fabio Votta <fabio.votta@gmail.com>
2020-11-20 18:01:22 +0100

    fix the id

--- R/info.R
+++ R/info.R
@@ -18,7 +18,7 @@ tk_info <- function(scope, query, ua = default_ua, port = NULL, vpn = F){
       get_data(url, ua = ua, port = port, vpn = vpn)
     },
     "music" = {
-      tk_posts(scope = "music", query = "6782187241935505410", n = 1, port = port, vpn = vpn, ua = ua)
+      tk_posts(scope = "music", query = query, n = 1, port = port, vpn = vpn, ua = ua)
     },
     "post" = {
       url <- get_url("post", query_1 = query)
